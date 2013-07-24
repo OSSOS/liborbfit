c9fbc36ebe7de3e16a6047d51c8e45daacb0af79
JJ Kavelaars <jj.kavelaars@nrc-cnrc.gc.ca>
2013-07-23 17:23:59 -0700

    added fit_radec as method

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 
 from distutils.core import setup, Extension
 
-ORBSUBS = ['orbfit1.c','transforms.c','dms.c','ephem_earth.c']
+ORBSUBS = ['orbfit1.c','orbfit2.c', 'transforms.c','dms.c','ephem_earth.c']
 NR      = ['nrutil.c', 'ludcmp.c', 'lubksb.c', 'gaussj.c', 'mrqmin_orbit.c',
            'mrqcof_orbit.c', 'covsrt.c', 'gasdev.c', 'ran1.c']
 
-module1= Extension('orbfit',
+module1= Extension('bk_orbfit',
                    sources=['orbfitmodule.c']+ORBSUBS+NR,
                    libraries=['m'])
 
-setup(name='orbfit',
-      version='1.0',
-      description='KBO orbfit return from G. Berstein',
+setup(name='bk_orbfit',
+      version='0.1',
+      description='Python binding on B&K Orbfit.',
       author='JJ Kavelaars',
-      author_email='jjk@hia.nrc.ca',
+      author_email='jj.kavelaars@nrc-cnrc.gc.ca',
       url='http://salish.dao.nrc.ca/~kavelaar/',
       ext_modules=[module1])
 
