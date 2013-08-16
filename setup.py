241c3a230c20237eb6f6993cc269b495a8c2930c
jjk <jj.kavelaars@nrc-cnrc.gc.ca>
2013-08-15 17:24:46 -0700

    not much

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
 
-from distutils.core import setup, Extension
-
-ORBSUBS = ['orbfit1.c','orbfit2.c', 'transforms.c','dms.c','ephem_earth.c']
-NR      = ['nrutil.c', 'ludcmp.c', 'lubksb.c', 'gaussj.c', 'mrqmin_orbit.c',
-           'mrqcof_orbit.c', 'covsrt.c', 'gasdev.c', 'ran1.c']
-
-module1= Extension('bk_orbfit',
-                   sources=['orbfitmodule.c']+ORBSUBS+NR,
-                   libraries=['m'])
+from distutils.core import setup
 
 setup(name='bk_orbfit',
       version='0.1',
       description='Python binding on B&K Orbfit.',
+      data_files=[('','data/binEphem.405'),
+                  ('','data/observatories.dat')],
       author='JJ Kavelaars',
+      package_dir=['bk_orbfit'],
       author_email='jj.kavelaars@nrc-cnrc.gc.ca',
       url='http://salish.dao.nrc.ca/~kavelaar/',
       ext_modules=[module1])
 
-                   
                    
                    
