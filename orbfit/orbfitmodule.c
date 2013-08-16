144cb052480163edd5346b0c8c27a040e468fec3
jjk <jj.kavelaars@nrc-cnrc.gc.ca>
2013-08-15 17:24:01 -0700

    now using ctypes

--- orbfit/orbfitmodule.c
+++ orbfit/orbfitmodule.c
@@ -1,51 +1,41 @@
-#include <Python.h>
-/* predict.c - Read a file containing a/b/g orbit fit, and spit out
- *  predicted RA & dec plus uncertainties on arbitrary date.
- * 8/12/99 gmb 
- */
 #include "orbfit.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-static PyObject *orbfit_fitradec(PyObject *self, PyObject *args)
-     //char *mpc_file
+int fitradec(char *mpc_filename, char *abg_filename, char *res_filename)
 {
-  const char *mpc_filename;
-  const char *abg_filename;
-  const char *res_filename;
 
-  FILE *abg_file;
-  FILE *mpc_file;
-  FILE *res_file;
+  FILE *abg_file ;
+  FILE *res_file ;
 
   OBSERVATION obsarray[MAXOBS];
   int     nobs;
 
-
   ORBIT orbit;
   PBASIS p;
   XVBASIS xv;
-  
+
+  double d, dd;
+
   double **covar;
   double chisq;
   int i; 
   int dof;
-
-  if (!PyArg_ParseTuple(args, "sss", &mpc_filename, &abg_filename, &res_filename ))
-    return NULL;
 
   covar = dmatrix(1,6,1,6);
 
 
   if (read_radec(obsarray, mpc_filename, &nobs)) {
     fprintf(stderr, "Error reading input observations\n");
-    return NULL ;
+    return -1 ;
   }
   
   /* Call subroutine to do the actual fitting: */
   fit_observations(obsarray, nobs, &p, covar, &chisq, &dof,stdout);
 
+
+  /* write the fit to the agbfile */
   abg_file = fopen(abg_filename,"w");
 
   fprintf(abg_file, "# Chi-squared of fit: %.2f DOF: %d\n",chisq,dof);
@@ -55,15 +45,12 @@ static PyObject *orbfit_fitradec(PyObject *self, PyObject *args)
   pbasis_to_bary(&p, &xv, NULL);
 
   orbitElements(&xv, &orbit);
-  fprintf(abg_file, "# a=%lf AU,e=%lf,i=%lf deg\n",orbit.a, orbit.e, orbit.i);
-  {
-    double d, dd;
-    d = sqrt(xBary*xBary + yBary*yBary + pow(zBary-1/p.g,2.));
-    dd = d*d*sqrt(covar[5][5]);
-    fprintf(abg_file, "# Barycentric distance %.3f+-%.3f\n",d,dd);
-  }
+  fprintf(abg_file, "# a=%f AU,e=%f,i=%f deg\n",orbit.a, orbit.e, orbit.i);
+  d = sqrt(xBary*xBary + yBary*yBary + pow(zBary-1/p.g,2.));
+  dd = d*d*sqrt(covar[5][5]);
+  fprintf(abg_file, "# Barycentric distance %.3f+-%.3f\n",d,dd);
 
-  /* Print the covariance matrix to stdout */
+  /* Print the covariance matrix to the agb_file */
   fprintf(abg_file, "# Covariance matrix: \n");
   print_matrix(abg_file,covar,6,6);
 
@@ -72,7 +59,9 @@ static PyObject *orbfit_fitradec(PyObject *self, PyObject *args)
   fprintf(abg_file, "%12.7f %12.7f %10.7f %10.7f %10.7f  %.6f\n",
 	 lat0/DTOR,lon0/DTOR,xBary,yBary,zBary,jd0);
 
-  /* Dump residuals to stderr */
+  fclose(abg_file);
+
+  /* Dump residuals to res_file */
   res_file = fopen(res_filename,"w");
   fprintf(res_file,"Best fit orbit gives:\n");
   fprintf(res_file,"obs  time        x      x_resid       y   y_resid\n");
@@ -84,21 +73,20 @@ static PyObject *orbfit_fitradec(PyObject *self, PyObject *args)
 	    obsarray[i].thetax/ARCSEC, (obsarray[i].thetax-x)/ARCSEC,
 	    obsarray[i].thetay/ARCSEC, (obsarray[i].thetay-y)/ARCSEC);
   }
+  fclose(res_file);
 
   free_dmatrix(covar,1,6,1,6);
-  return Py_BuildValue("fffff",orbit.a, orbit.e, orbit.i, orbit.lan, orbit.aop, orbit.T);
+  return nobs;
+}
 
-} 
 
-  
-  
+/* predict.c - Read a file containing a/b/g orbit fit, and spit out
+ *  predicted RA & dec plus uncertainties on arbitrary date.
+ * 8/12/99 gmb
+ */
 
-static PyObject *orbfit_predict(PyObject *self, PyObject *args)
-     //char *abg_file, float jdate, int obscode)
+double *predict(char *abg_file, float jdate, int obscode)
 {
-  const char *abg_file;
-  float jdate;
-  int obscode;
 
   PBASIS p;
   OBSERVATION	futobs;
@@ -111,11 +99,9 @@ static PyObject *orbfit_predict(PyObject *self, PyObject *args)
   double ra,dec, **coveq;
   double yr,mo,day,hr,mn,ss;
   double xx,yy,xy,bovasqrd,det;
+  static double result[5];
   int i,nfields;
   int iarg=1;
-
-  if (!PyArg_ParseTuple(args,"sfi",&abg_file, &jdate, &obscode))
-    return NULL;
 
   sigxy = dmatrix(1,2,1,2);
   derivs = dmatrix(1,2,1,2);
@@ -123,11 +109,18 @@ static PyObject *orbfit_predict(PyObject *self, PyObject *args)
   covecl = dmatrix(1,2,1,2);
   coveq = dmatrix(1,2,1,2);
 
+  result[0] = -1.0;
+  result[1] = -1.0;
+  result[2] = -1.0;
+  result[3] = -1.0;
+  result[4] = -1.0;
  
   if (read_abg(abg_file,&p,covar) ) { 
     fprintf(stderr, "Error input alpha/beta/gamma file %s\n",abg_file);
-    return NULL;
+    return result;
   }
+
+
 
   /* get observatory code */
   futobs.obscode=obscode;
@@ -137,21 +130,6 @@ static PyObject *orbfit_predict(PyObject *self, PyObject *args)
 
   predict_posn(&p,covar,&futobs,sigxy);
 
-  /*  This is for the sky plane stuff... */
-  /* Compute a, b, theta of error ellipse for output */
-  // xx = sigxy[1][1];
-  // yy = sigxy[2][2];
-  // xy = sigxy[1][2];
-  // PA = 0.5 * atan2(2.*xy,(xx-yy)) * 180./PI;	/*go right to degrees*/
-  /* Adjust for PA to be N through E, */
-  // PA = PA-90;
-  // if (PA<-90.) PA += 180.;
-
-  // bovasqrd  = (xx+yy-sqrt(pow(xx-yy,2.)+pow(2.*xy,2.))) 
-  // (xx+yy+sqrt(pow(xx-yy,2.)+pow(2.*xy,2.))) ;
-  // det = xx*yy-xy*xy;
-  //b = pow(det*bovasqrd,0.25);
-  // a = pow(det/bovasqrd,0.25);
 
   
   /* Now transform to RA/DEC, via ecliptic*/
@@ -184,22 +162,14 @@ static PyObject *orbfit_predict(PyObject *self, PyObject *args)
   dec /= DTOR;
 
 
-  return Py_BuildValue("fffff",ra,dec,a/ARCSEC,b/ARCSEC,PA);
+   result[0] = ra;
+   result[1] = dec;
+   result[2] = a/ARCSEC;
+   result[3] = b/ARCSEC;
+   result[4] = PA;
 
-}
+  return result;
 
-
-static PyMethodDef OrbfitMethods[] = {
-    {"predict",  orbfit_predict, METH_VARARGS,
-     "Given an ABG_FILE, JDATE, OBS_CODE return the objects location."},
-    {"fit_radec", orbfit_fitradec, METH_VARARGS,
-     "Given an MPC_FILE as input write to ABG_FILE and RES_FILE."},
-    {NULL, NULL, 0, NULL}        /* Sentinel */
-};
-
-PyMODINIT_FUNC initbk_orbfit(void)
-{
-  (void) Py_InitModule("bk_orbfit",OrbfitMethods);
 }
 
 
