f7a0d4643b6937a42e59880c490631563af8931d
JJ Kavelaars <jjk@uvic.ca>
2016-07-05 20:00:15 -0700

    MAde the example a bit more interesting

--- example.py
+++ example.py
@@ -1,11 +1,14 @@
-import mp_ephem
+from mp_ephem import *
 from astropy import units
 import sys
 
 mpc_filename = sys.argv[1]
-obs = mp_ephem.EphemerisReader().read(mpc_filename)
-orb = mp_ephem.BKOrbit(obs)
+obs = EphemerisReader().read(mpc_filename)
+orb = BKOrbit(obs)
 print orb.summarize()
-orb.predict(orb.time + 10 * units.day)
-print orb.coordinate.hmsdms, orb.distance
+date = orb.time
+for day in range(100):
+    orb.predict(orb.time + 10 * units.day)
+    print "{} {} {:5.2f}".format(orb.time.mpc, orb.coordinate.to_string('hmsdms', precision=2, sep=":"), orb.distance)
+
 
